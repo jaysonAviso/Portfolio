8194cca30ae2f0e36d6d340ca92717218c20d9e0
jaysonAviso <jayson.aviso1@gmail.com>
2022-09-28 23:54:04 +0800

    Adding sidenav

--- FrontEnd/src/app/header/header.component.ts
+++ FrontEnd/src/app/header/header.component.ts
@@ -1,3 +1,4 @@
+import { debugOutputAstAsTypeScript } from '@angular/compiler';
 import { Component, OnInit, Output } from '@angular/core';
 
 @Component({
@@ -7,9 +8,51 @@ import { Component, OnInit, Output } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 @Output() hiden = false;
+toggle = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  navToggle() {
+    const sideNav = document.getElementById('sideNav');
+    
+    if(!this.toggle) {
+      const sideMenu = document.getElementById('sideNav');
+      sideMenu.style.width='100%';
+      sideMenu.style.height='100vh';
+      sideMenu.style.display='flex';
+      this.toggle = true;
+    } else {
+      const sideMenu = document.getElementById('sideNav');
+      sideMenu.style.width='0';
+      sideMenu.style.height='0';
+      sideMenu.style.display='none';
+      this.toggle = false;
+    }
+
+    // console.log(this.toggle);
+
+  }
+
+  openSideMenu() {
+    const sideMenu = document.getElementById('sideNav');
+    const buttonMenu = document.getElementById('buttonMenu');
+
+    sideMenu.style.width='100%';
+    sideMenu.style.height='100vh';
+    buttonMenu.style.display='none';
+  }
+
+  closeSideMenu() {
+    const sideMenu = document.getElementById('sideNav');
+    const buttonMenu = document.getElementById('buttonMenu');
+
+    sideMenu.style.width='0';
+    sideMenu.style.height='0';
+    sideMenu.style.display='none';
+    buttonMenu.style.display='contents';
   }
 
 }
