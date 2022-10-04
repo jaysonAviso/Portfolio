8b9d0e060ca6fc6d5d36bdf252405b2390f486e9
jaysonAviso <jayson.aviso1@gmail.com>
2022-10-04 13:29:02 +0800

    debug

--- API/Controllers/FallbackController.cs
+++ API/Controllers/FallbackController.cs
@@ -9,7 +9,8 @@ namespace API.Controllers
     public class FallbackController : Controller
     {
         public ActionResult Index() {
-            return PhysicalFile(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "index.html"), "text/HTML");
+            return PhysicalFile(Path.Combine(Directory.GetCurrentDirectory(), 
+                "wwwroot", "index.html"), "text/HTML");
         }   
     }
 }
